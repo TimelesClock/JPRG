d26d64056e57f44b5253e56543ba9acf21a0943e
TimelesClock <leongandy027@gmail.com>
2022-10-20 13:41:10 +0800

    First Commit

---
+++ src/Practical1/Initials.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practical1;
+
+/**
+ *
+ * @author leong
+ */
+public class Initials {
+    public static void main(String[] args){
+        System.out.println("  *   *    \n ***  *    \n*   * *****");
+    }
+}
