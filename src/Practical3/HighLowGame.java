7279b1e4deea4460e29053726603f0c9f3cb7d65
Andy Leong <leongandy.22@ichat.sp.edu.sg>
2022-10-28 08:37:36 +0100

    Q3

---
+++ src/Practical3/HighLowGame.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practical3;
+import java.util.Random;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author leong
+ */
+public class HighLowGame {
+    
+    int randomNumber;
+    int guess;
+    boolean gameEnd = false;
+    
+    public void generateRandomNumber(){
+        Random rand = new Random();
+        randomNumber = rand.nextInt(100);
+    }
+    
+    public void getUserInput(){
+        String in = JOptionPane.showInputDialog(null,"Enter your guess.");
+        guess = Integer.parseInt(in);
+    }
+    
+    public String getResult(){
+        String result;
+        if (guess > randomNumber){
+            result = String.format("%d is higher than the Magic Number",guess);
+        }else if (guess < randomNumber){
+            result = String.format("%d is lower than the Magic Number",guess);
+        }else{
+            result = String.format("%d is the Magic Number",guess);
+            gameEnd = true;
+        }
+        return result;
+    }
+    
+    public boolean gameEnded(){
+        return gameEnd;
+    }
+}
---
+++ src/Practical3/TestHighLowGame.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practical3;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author leong
+ */
+public class TestHighLowGame extends HighLowGame{
+
+    public static void main(String[] args) {
+        HighLowGame game = new HighLowGame();
+        JOptionPane.showMessageDialog(null,"Welcome to The High Low Number Game");
+        
+        //Create an instance of the HighLowGame called game
+        //Invoke generateRandomNumber() to generate random Number
+        game.generateRandomNumber();
+        do {
+            game.getUserInput();
+            JOptionPane.showMessageDialog(null,game.getResult());
+        //Invoke getUserInput()
+        //Display the result by invoking getResult()
+        } while (!game.gameEnded());
+    }
+}
