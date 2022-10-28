01fd3f0efafaf59fa595014309f479737646d2ce
Andy Leong <leongandy.22@ichat.sp.edu.sg>
2022-10-28 07:18:52 +0100

    Q4 update

--- src/Practical2/Q4.java
+++ src/Practical2/Q4.java
@@ -14,60 +14,63 @@ public class Q4 {
 
     public static void main(String[] args) {
         String output;
-        String UserInput = JOptionPane.showInputDialog(
-                null,
-                "Enter a numeric value:",
-                "Input",
-                JOptionPane.QUESTION_MESSAGE
-        );
-        switch (UserInput) {
-            case "1":
-                output = "I";
-                break;
-            case "2":
-                output = "II";
-                break;
-            case "3":
-                output = "III";
-                break;
-            case "4":
-                output = "IV";
-                break;
-            case "5":
-                output = "V";
-                break;
-            case "6":
-                output = "VI";
-                break;
-            case "7":
-                output = "VII";
-                break;
-            case "8":
-                output = "VIII";
-                break;
-            case "9":
-                output = "IX";
-                break;
-            case "10":
-                output = "X";
-                break;
-            default:
-                output = "no";
-        }
-        if (output != "no") {
-            JOptionPane.showMessageDialog(
+        while (true){
+            String UserInput = JOptionPane.showInputDialog(
                     null,
-                    "Roman Numeral Value = " + output,
-                    "Message",
-                    JOptionPane.INFORMATION_MESSAGE
+                    "Enter a numeric value:",
+                    "Input",
+                    JOptionPane.QUESTION_MESSAGE
             );
-        } else {
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Out of Range! Pls enter a number between 1 and 10!",
-                    "Error",
-                    JOptionPane.ERROR_MESSAGE
-            );
+            switch (UserInput) {
+                case "1":
+                    output = "I";
+                    break;
+                case "2":
+                    output = "II";
+                    break;
+                case "3":
+                    output = "III";
+                    break;
+                case "4":
+                    output = "IV";
+                    break;
+                case "5":
+                    output = "V";
+                    break;
+                case "6":
+                    output = "VI";
+                    break;
+                case "7":
+                    output = "VII";
+                    break;
+                case "8":
+                    output = "VIII";
+                    break;
+                case "9":
+                    output = "IX";
+                    break;
+                case "10":
+                    output = "X";
+                    break;
+                default:
+                    output = "no";
+            }
+            if (output != "no") {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Roman Numeral Value = " + output,
+                        "Message",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+                break;
+            } else {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Out of Range! Pls enter a number between 1 and 10!",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
         }
     }
 }
