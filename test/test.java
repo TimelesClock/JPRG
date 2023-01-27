ce1a450ecc1e226cf6ea5d08f7a8a2477b696f8e
Andy Leong <leongandy.22@ichat.sp.edu.sg>
2023-01-27 06:10:46 +0000

    P5

---
+++ test/test.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leong
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int a = 10, b = 2, c = 56, d = 1;
+        int[] arrInt = {30, 41, 15, 3, 59};
+        double h = 45.7;
+        h = (int) h / 4;
+        boolean x
+                = arrInt.length != 5 || false && true;
+        d += arrInt[1] % arrInt[3];
+        int w = a++ * --b + ++c;
+        boolean z = true && !false || false;
+        System.out.println("h is " + h);
+        System.out.println("x is " + x);
+        System.out.println("d is " + d);
+        System.out.println("w is " + w);
+        System.out.println("z is " + z);
+    }
+}
